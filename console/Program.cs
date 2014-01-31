3f17108e0187ede040a83557eac3e6ac4747079f
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2014-01-31 22:32:29 +0400

    Access through mono's Sqlite data provider

--- console/Program.cs
+++ console/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
 
 namespace console
 {
@@ -6,7 +9,32 @@ namespace console
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			try
+			{
+				var settings = ConfigurationManager.ConnectionStrings ["main_db"];
+				Console.WriteLine ("ProviderName: {0}", settings.ProviderName);
+				Console.WriteLine ("Connection string: {0}", settings.ConnectionString);
+				var factory = DbProviderFactories.GetFactory (settings.ProviderName);
+				using (var connection = factory.CreateConnection ())
+				{
+					connection.ConnectionString = settings.ConnectionString;
+					connection.Open ();
+					var command = connection.CreateCommand ();
+					command.CommandText = "SELECT * FROM Messages";
+					using (var reader = command.ExecuteReader ())
+					{
+						while (reader.Read ())
+						{
+							var xml = reader ["body_xml"];
+							Console.WriteLine (xml);
+						}
+					}
+				}
+			} catch (Exception ex)
+			{
+				Console.WriteLine (ex.ToString ());
+			}
 		}
 	}
 }
+
